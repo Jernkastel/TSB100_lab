64982ca9fde47d59895ae3a4e09154dae057f220
Mask Järn <47307674+Jernkastel@users.noreply.github.com>
2019-04-23 05:43:22 +0200

    Implementing webservices. WCF is implemented but only returns null values. REST service is about to be implemented.

---
+++ TSB100_lab/Controllers/WCFDiaryController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TSB100_lab.Controllers
+{
+    public class WCFDiaryController : Controller
+    {
+        // GET: WCFDiary
+        public ActionResult Index()
+        {
+            WCFDiaryService.WCFDiaryServiceClient client = new WCFDiaryService.WCFDiaryServiceClient();
+            List<WCFDiaryService.WCFDiaryServiceData> wcfDiaryList = client.GetDiaryData().ToList();
+            return View(wcfDiaryList);
+        }
+    }
+}
